fc76ba903f02fa5c4325398ef374c4670244279e
faith2221 <edithbanda014@gmail.com>
2023-10-23 04:44:02 -0700

    Header file

--- sort.h
+++ sort.h
@@ -39,6 +39,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void swap_nodes(listint_t **list, listint_t *node);
 
 /**************************************
  * Advanced Tasks Function prototypes *
